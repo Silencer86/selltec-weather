49925da5149c1d3b84794ee1bc71f357b0e6bf79
Volker Ströbel <vs@selltec.com>
2020-02-18 16:05:10 +0100

    [wetterstation] Revert full implementation

--- wetterstation/src/main/java/wetterstation/Tag.java
+++ wetterstation/src/main/java/wetterstation/Tag.java
@@ -5,17 +5,4 @@ class Tag {
     private int tagImMonat;
     private int temperatur;
 
-    public Tag(int tagImMonat, int temperatur) {
-        this.tagImMonat = tagImMonat;
-        this.temperatur = temperatur;
-    }
-
-    public int getTagImMonat() {
-        return tagImMonat;
-    }
-
-    public int getTemperatur() {
-        return temperatur;
-    }
-
 }
--- wetterstation/src/main/java/wetterstation/WetterStation.java
+++ wetterstation/src/main/java/wetterstation/WetterStation.java
@@ -1,6 +1,5 @@
 package wetterstation;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class WetterStation {
@@ -8,73 +7,24 @@ class WetterStation {
     private List<Tag> tage;
 
     public WetterStation() {
-        tage = new ArrayList<>();
     }
 
     public void addTag(int temperatur) {
-        tage.add(new Tag(tage.size() + 1, temperatur));
     }
 
     public List<Tag> getTage() {
-        return tage;
     }
 
     public double getDurchschnitt() {
-        double summe = 0;
-
-        for (Tag tag : tage) {
-            summe += tag.getTemperatur();
-        }
-
-        return summe / tage.size();
     }
 
     public int getMax() {
-        Integer max = null;
-
-        for (Tag tag : tage) {
-            if (max == null) {
-                max = tag.getTemperatur();
-            } else {
-                max = Math.max(max, tag.getTemperatur());
-            }
-        }
-
-        return max;
     }
 
     public int getMin() {
-        Integer min = null;
-
-        for (Tag tag : tage) {
-            if (min == null) {
-                min = tag.getTemperatur();
-            } else {
-                min = Math.min(min, tag.getTemperatur());
-            }
-        }
-
-        return min;
     }
 
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("Temp:");
-
-        for (Tag tag : tage) {
-            sb.append("\t");
-            sb.append(tag.getTemperatur());
-        }
-
-        sb.append("\nTag: ");
-
-        for (Tag tag : tage) {
-            sb.append("\t");
-            sb.append(tag.getTagImMonat());
-        }
-
-        return sb.toString();
     }
 
 }
